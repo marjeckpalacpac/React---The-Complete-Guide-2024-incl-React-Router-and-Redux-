f375a18c8fece03a79a85207b1bcc87d5edea39f
Marjeck Palacpac <marjeck@advabilities.com>
2024-03-31 14:44:14 +0800

    How NOT to Update the UI - A Look Behind The Scenes of React [Core Concept]

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,11 @@ import TabButton from "./components/TabButton.jsx";
 
 
 function App() {
+  let tabContent = 'Please click a button';
+
   function handleSelect(selectedButton) {
     // selectedButton  => 'components', 'jsx', 'props', 'state'
-    console.log(selectedButton)
+    tabContent = selectedButton
   }
 
 
@@ -40,6 +42,7 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
+          {tabContent}
         </section>
       </main>
     </div>
